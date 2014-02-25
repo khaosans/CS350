5f85ec865509cc6b0b528e798f31ca07cfa82829
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-02-25 08:40:27 -0800

    updated program to include cleaner test for fic and dutch

--- dutch.py
+++ dutch.py
@@ -1,36 +1,59 @@
+#Problem #4
+#Random is used for testing the program
 import random
 
 __author__ = 'souriyakhaosanga'
 
-def dutch_national_flag(arrayInput, length):
-    high = length - 1
+#This method is pretty much sorting the array.
+#
+def dutch_national_flag(arrayInput):
+    maxOfArray = arrayInput.__len__() - 1
     p = 0
     i = 0
-    while i <= high:
+    while i <= maxOfArray:
         if arrayInput[i] == 0:
-            #swap
+            #Perform Swap of the array indexes
             arrayInput[i], arrayInput[p] = arrayInput[p], arrayInput[i]
             p += 1
             i += 1
         elif arrayInput[i] == 2:
-            #swap
-            arrayInput[i], arrayInput[high] = arrayInput[high], arrayInput[i]
-            high -= 1
+            #Perform swap of the array indexes
+            arrayInput[i], arrayInput[maxOfArray] = arrayInput[maxOfArray], arrayInput[i]
+            maxOfArray -= 1
         else:
             i += 1
 
-
-def main():
-    #create a stack of random numbers
+#Method is used for building array of random colors of the
+#dutch national flag
+def build_random_array(size):
     stack = []
     i = 0
-    while i <= 35:
+    while i <= size:
+        #create random numbers between 0-2 to simulate the three colors
+        #of the ducth national flag
         stack.append(random.randint(0, 2))
         i += 1
+    return stack
 
-    print "Input: ", stack
-    dutch_national_flag(stack, len(stack))
-    print "Sorted: ", stack
+def print_test(stack):
+    print "input: ", stack
+    dutch_national_flag(stack)
+    print "after: ", stack
+
+
+def main():
+
+    print "Test 1"
+    stack = build_random_array(40)
+    print_test(stack)
+
+    print "Test 2"
+    stack1 = build_random_array(10)
+    print_test(stack1)
+
+    print "Test 3"
+    stack2 = build_random_array(5)
+    print_test(stack2)
 
 if __name__ == "__main__":
     main()
--- fic.py
+++ fic.py
@@ -1,7 +1,15 @@
 __author__ = 'souriyakhaosanga'
 
+#This is problem number #2
+
+
+
 #Time efficiency is big O(mn)
 #Method is used to find mutual friends
+#This pretty much just compares each index in
+#the array1 to each index of array2.
+#It is actually pretty slow compare to the 2nd faster
+#method
 def brute_force(array1, array2):
     mutual = []
     for i in array1:
@@ -11,7 +19,15 @@ def brute_force(array1, array2):
     print mutual
 
 #
-#Friends in common log n
+#The time efficiency of this function is O(n log n)
+#The method is much faster as that it uses 4 arrays
+#The array1 is concatenated directly onto the array2
+#which is called array c
+#Afterwhich, c is sorted.  Python has a built in
+#sort called tim sort. Cited here http://en.wikipedia.org/wiki/Timsort
+#Then after each index in the sorted array is compared to it's
+#adjacent member in the array.  If there is a match, it is saved
+#and copied over to the d array--this is the in commons array.
 def faster(array1, array2):
     c = []
     d = []
@@ -27,11 +43,14 @@ def faster(array1, array2):
 def main():
     print "program for mutual friends"
 
-    person1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    person2 = [2, 5, 7, 8, 9]
+    person1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    person2 = [2, 5, 7, 8, 9, 3]
+    print "input 1: ", person1
+    print "input 2: ", person2
 
+    print "Brute force: "
     brute_force(person1, person2)
-
+    print "None brute: "
     faster(person1, person2)
 
 
