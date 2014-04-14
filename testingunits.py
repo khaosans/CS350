1d3f638f83efe09db096993c15a39c8a64183910
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-04-14 13:29:25 -0700

    added dynamic binding

---
+++ testingunits.py
@@ -0,0 +1,30 @@
+import random
+import unittest
+
+__author__ = 'souriyakhaosanga'
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_shuffle(self):
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+        self.assertRaises(TypeError, random.shuffle, (1, 2, 3))
+
+    def test_choices(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
+
+if __name__ == '__main__':
+    unittest.main()
